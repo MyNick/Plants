bdef23e3a3a4c298e4e2aad47cd3a5fb78e1e055
ehoodsentra <ehood@sentra.io>
2023-03-14 01:50:03 +0200

    add plant cards

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,29 @@
+import { Box, Paper, Typography } from '@mui/material';
+
+const plants = ['Cusbara', 'Petruzilia', 'Marva', 'Rozmarin', 'Timin', 'Nana'];
+
 export default function App() {
-  return <div className="App">a</div>;
+  return (
+    <Box width="100%" height="100vh">
+      <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 2, padding: 2 }}>
+        {plants.map((plant) => {
+          return (
+            <Paper
+              key={plant}
+              elevation={4}
+              sx={{
+                minWidth: 300,
+                minHeight: 300,
+                display: 'flex',
+                flexDirection: 'column',
+              }}
+            >
+              <Box sx={{ marginX: 'auto' }}>{plant}</Box>
+              <Box>Here will be the graph</Box>
+            </Paper>
+          );
+        })}
+      </Box>
+    </Box>
+  );
 }
