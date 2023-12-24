7278c9aa2443c9bf369141ac4ced23f41edaeff5
Itay Goren <itaygo@vortex-imaging.com>
2023-12-24 22:47:26 +0200

    .

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,17 @@
+import path from "path"
+import glob from "glob"
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: path.join(__dirname, "src"),
   server: { port: 8000 },
+  build: {
+    outDir: path.join(__dirname, "dist"),
+    rollupOptions: {
+        input: glob.sync(path.resolve(__dirname, "src", "*.html")),
+    }
+  }
 });
